d544cf3f962ca706cb9fbf7f43069cdc12bf72f6
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-17 13:53:57 -0400

    created funciton and input

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,4 +1,15 @@
+
+var input;
 //create function
+function myFunction(test){
+  console.log(test);
+}
+
+input = prompt("Papers please?");
+console.log(input);
+myFunction(imput);
+
+
   //control and return statements
 
 //Prompt user for input and store variable
